66f857230fc66e63cb0fe55daabd03df7dd2733f
Noel-H <noel.huberto@gmail.com>
2019-02-26 22:51:54 +0100

    Génération et comparaison avec des tableau réussi en brouillon

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,133 @@
+
 public class Main {
 
     static public void main(String... args) {
 
-        //Menu.principalMenu();
+//        Menu.principalMenu();
 
-        int resultatScanner=4;
-        int numberToFind[]= new int[resultatScanner];
-        String numberString="";
-        int numberInt;
-        int j = resultatScanner;
-        int g=10;
-        int d=1;
 
-        for(int i=0; i < numberToFind.length; i++){
-            numberToFind[i]=(int) (Math.random() * 10);
-            System.out.println("À l'emplacement " + i +" du tableau nous avons = " + numberToFind[i]);
+        int resultatScanner = 10;
+        int numberToFind[] = new int[resultatScanner];
+        String numberString = "";
+
+
+        for (int i = 0; i < numberToFind.length; i++) {
+            numberToFind[i] = (int) (Math.random() * 10);
+            System.out.println("À l'emplacement " + i + " du tableau nous avons = " + numberToFind[i]);
             String conversion = Integer.toString(numberToFind[i]);
-            numberString=numberString+conversion;
+            numberString = numberString + " | "+conversion;
+
+        }
+        System.out.println(numberString);
+
+
+        String playerReponse = "0123456789";
+
+        String playerResponseTableau[] = new String[resultatScanner];
+
+        String numberResponseString="";
+
+        int numberIntResponseTableau[] = new int[resultatScanner];
+
+//        playerResponseTableau[0] = Character.toString(playerReponse.charAt(0));
+//
+//        System.out.println(playerResponseTableau[0]);
+
+
+        for (int i = 0; i < playerResponseTableau.length; i++) {
+            playerResponseTableau[i] = Character.toString(playerReponse.charAt(i));
+            numberIntResponseTableau[i] = Integer.parseInt(playerResponseTableau[i]);
+            System.out.println("Tableau String = "+playerResponseTableau[i]);
+            System.out.println("Tableau Int = "+numberIntResponseTableau[i]);
+            numberResponseString=numberResponseString+" | "+playerResponseTableau[i];
+
+        }
+
+        //System.out.println(numberResponseString);
+
+
+        String resultat[]=new String[resultatScanner];
+        String resultatEnString="";
+
+        for (int i = 0; i < playerResponseTableau.length; i++) {
+            if (numberToFind[i]>numberIntResponseTableau[i]){
+                resultat[i]="+";
+            }else if (numberToFind[i]<numberIntResponseTableau[i]){
+                resultat[i]="-";
+            }else if (numberToFind[i]==numberIntResponseTableau[i]){
+                resultat[i]="=";
+            }else {
+                System.out.println("Error");
+            }
+
+            resultatEnString=resultatEnString+" | "+resultat[i];
 
         }
 
         System.out.println(numberString);
-        numberInt=Integer.parseInt(numberString);
-        System.out.println(numberInt);
+        System.out.println(numberResponseString);
+        System.out.println(resultatEnString);
 
 
-        int playerResultat=3211;
+//
+//        for (int i = 0; i < playerResponseTableau.length; i++) {
+//            playerResponseTableau[i] = Arrays.asList(playerResponseTableau).stream().mapToInt(Integer::parseInt).toArray();
+//
+//            System.out.println(playerResponseTableau[i]);
+//        }
 
 
 
-        int resultat[]= new int[resultatScanner];
-        int resultattest[]= new int[resultatScanner];
+
+//        int resultatScanner=4;
+//        int numberToFind[]= new int[resultatScanner];
+//        String numberString="";
+//        int numberInt;
+//        int j = resultatScanner;
+//        int g=10000;
+//        int d=1000;
+//
+//        for(int i=0; i < numberToFind.length; i++){
+//            numberToFind[i]=(int) (Math.random() * 10);
+//            System.out.println("À l'emplacement " + i +" du tableau nous avons = " + numberToFind[i]);
+//            String conversion = Integer.toString(numberToFind[i]);
+//            numberString=numberString+conversion;
+//
+//        }
+//
+//        System.out.println(numberString);
+//
+//        int playerResultat=0115;
+//
+//        int resulat[]= new int[resultatScanner];
+//
+//        for (int i=resultatScanner-1;i>-1;i--){
+//
+//            resulat[i]=(playerResultat%g)/d;
+//
+//            System.out.println(resulat[i]);
+//
+//            g=g/10;
+//            d=d/10;
+//        }
 
 
-        while (j>=0){
+//        resultat[0]=(playerResultat%10000)/1000;
+//
+//        System.out.println(resultat[0]);
+//
+//        g=g*10;
+//        d=d*10;
+//
+//        g=g*10;
+//        d=d*10;
+//
+//        g=g*10;
+//        d=d*10;
+//
+//        System.out.println(g);
+//        System.out.println(d);
 
-        resultattest[j]  = (playerResultat % g)/d;
-
-            System.out.println(resultattest[j]);
-    j--;
-    g=g*10;
-    d=d*10;
-
-}
 
         //String str = "1";
         //str = str+"2";
