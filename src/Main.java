a4bca9e20a42ee52547e597b782de736df34a650
Noel-H <noel.huberto@gmail.com>
2019-02-26 13:59:40 +0100

    Recherche de solution car si un 0 est présent dans la saisie du joueur le résultat sera faussé

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,51 @@ public class Main {
 
     static public void main(String... args) {
 
-        Menu.principalMenu();
+        //Menu.principalMenu();
+
+        int resultatScanner=4;
+        int numberToFind[]= new int[resultatScanner];
+        String numberString="";
+        int numberInt;
+        int j = resultatScanner;
+        int g=10;
+        int d=1;
+
+        for(int i=0; i < numberToFind.length; i++){
+            numberToFind[i]=(int) (Math.random() * 10);
+            System.out.println("À l'emplacement " + i +" du tableau nous avons = " + numberToFind[i]);
+            String conversion = Integer.toString(numberToFind[i]);
+            numberString=numberString+conversion;
+
+        }
+
+        System.out.println(numberString);
+        numberInt=Integer.parseInt(numberString);
+        System.out.println(numberInt);
+
+
+        int playerResultat=3211;
+
+
+
+        int resultat[]= new int[resultatScanner];
+        int resultattest[]= new int[resultatScanner];
+
+
+        while (j>=0){
+
+        resultattest[j]  = (playerResultat % g)/d;
+
+            System.out.println(resultattest[j]);
+    j--;
+    g=g*10;
+    d=d*10;
+
+}
+
+        //String str = "1";
+        //str = str+"2";
+        //System.out.println(str);
 
     }
 }
